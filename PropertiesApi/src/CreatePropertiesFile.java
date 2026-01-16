bab8836448bfd6835019f10647803337060983c0
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-15 23:42:47 -0300

    Creating a properties file

---
+++ PropertiesApi/src/CreatePropertiesFile.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class CreatePropertiesFile {
+    public static void main(String[] args) throws Exception {
+        // create an instance of Properties
+        Properties p = new Properties();
+
+        // add properties to it
+        p.setProperty("name", "Mateus Lopes");
+        p.setProperty("email",
+                "mateuslopes92@gmail.com");
+
+        // store the properties to a file
+        p.store(new FileWriter("src/info.properties"),
+                "Creating properties example");
+    }
+}
