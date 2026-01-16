c3467ef0a05955bd7fae2e1a050734b797bfd175
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-15 23:44:00 -0300

    Reading properties file

---
+++ PropertiesApi/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // create a reader object on the properties file
+        FileReader reader = new FileReader("src/info.properties");
+
+        // create properties object
+        Properties p = new Properties();
+
+        // Add a wrapper around reader object
+        p.load(reader);
+
+        // access properties data
+        System.out.println(p.getProperty("name"));
+        System.out.println(p.getProperty("password"));
+    }
+}
