c359005d6140061dcc9a4e3a4b0e06b15b713636
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-21 23:40:07 -0300

    Setting fields of a class

--- ReflectionApi/src/Main.java
+++ ReflectionApi/src/Main.java
@@ -1,7 +1,7 @@
 import java.lang.reflect.Field;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Cat myCat = new Cat("Mary", 6);
 
         // Getting all the fields declared in the Cat class
@@ -12,6 +12,21 @@ public class Main {
         }
         System.out.println("--------------------");
 
-    
+        System.out.println();
+
+        // Setting objects (using reflection to force the change even private/final prop)
+        System.out.println("-----Cat Set Fields-----");
+        for(Field field: catFields){
+            if(field.getName().equals("name")){
+                // making field accessible
+                field.setAccessible(true);
+
+                // pass the object and the value
+                // java make handle the exception so is need to add throws in the main method
+                field.set(myCat, "Jayne");
+            }
+        }
+        System.out.println("Name now is = " + myCat.getName());
+        System.out.println("--------------------");
     }
 }
