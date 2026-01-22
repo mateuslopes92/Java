cdc5b12a02ecd7262e794c568262fe38eaf69dd1
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-21 23:34:13 -0300

    Getting fields from a class

--- ReflectionApi/src/Main.java
+++ ReflectionApi/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Field;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Cat myCat = new Cat("Mary", 6);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Getting all the fields declared in the Cat class
+        Field[] catFields = myCat.getClass().getDeclaredFields();
+        System.out.println("-----Cat Get Fields-----");
+        for(Field field: catFields){
+            System.out.println(field.getName());
         }
+        System.out.println("--------------------");
+
+    
     }
 }
