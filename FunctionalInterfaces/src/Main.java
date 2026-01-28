2797611cee6aa67d7838967fa88c2046d9bd083e
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-27 22:39:27 -0300

    Functional interfaces: Function

--- FunctionalInterfaces/src/Main.java
+++ FunctionalInterfaces/src/Main.java
@@ -23,6 +23,11 @@ public class Main {
         Runnable rnb = () -> System.out.println("Runnable: no params no return");
         rnb.run();
 
+        // Function: receives a parameter type and return type
+        // Can be any type: Function<Integer, String> test = x -> "Test is " + x;
+        Function<Integer, Integer> fnc = x -> x*2;
+        Integer fncResult = fnc.apply(2);
+        System.out.println("Function: receives a parameter and return the defined type: " + fncResult);
 
     }
 }
