87d8ae17101f73ff7653ee5a1a46482fd48214e9
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-27 22:38:49 -0300

    Lambda and runnable

--- FunctionalInterfaces/src/Main.java
+++ FunctionalInterfaces/src/Main.java
@@ -1,6 +1,28 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
+//        MyFunctionalInterface mfi = new MyFunctionalInterface() {
+//            @Override
+//            public void run() {
+//                System.out.println("Running method");
+//            }
+//        };
+//
+//        mfi.run();
+
+        // implementing in simple way the run function
+        MyFunctionalInterface mfi = () -> System.out.println("Hello lambda");
+        mfi.run();
+
+        /**
+         * Types of functional interfaces from java.util.function*
+         */
+        //Runnable dont receives parameter and dont return nothing
+        Runnable rnb = () -> System.out.println("Runnable: no params no return");
+        rnb.run();
+
 
     }
 }
