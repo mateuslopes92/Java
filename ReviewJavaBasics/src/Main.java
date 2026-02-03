1a7be04b9f8899030ac55f98822873edbfb129d3
mateuslopes92 <mateuslopes92@gmail.com>
2026-02-02 23:01:18 -0300

    JavaBasics: Refactor to study OOP again

--- ReviewJavaBasics/src/Main.java
+++ ReviewJavaBasics/src/Main.java
@@ -2,18 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Primitive Data Types
-        int number = 10;
-        double price = 9.99;
-        char grade = 'A';
-        boolean isJavaFun = true;
 
-        // Non-Primitive Data Types
-        String greeting = "Hello";
-        int[] numbers = {1, 2, 3, 4, 5};
-
-        int greetingLength = greeting.length();
-
-        System.out.println(greetingLength);
     }
 }
