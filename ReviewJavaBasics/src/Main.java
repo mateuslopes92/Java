14186504491f2f09bb603421edb25e0a3e8bcf85
mateuslopes92 <mateuslopes92@gmail.com>
2026-02-02 23:11:59 -0300

    JavaBasics: Encapsulation with classes and objects

--- ReviewJavaBasics/src/Main.java
+++ ReviewJavaBasics/src/Main.java
@@ -2,6 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Inventory inventory = new Inventory();
 
+        Item macbook = new Item(
+    "Macbook",
+    2
+        );
+        Item iPhone = new Item(
+    "iPhone 16 Pro Max",
+    1
+        );
+
+        inventory.addItem(macbook);
+        inventory.addItem(iPhone);
+
+        inventory.displayInventory();
     }
 }
