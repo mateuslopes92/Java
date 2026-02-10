ecea872b0d976570884ddde42f7a45e7cac6a1a5
mateuslopes92 <mateuslopes92@gmail.com>
2026-02-09 23:43:16 -0300

    ConcurrencyExecutorService: Tyes of tasks(ScheduledThreadPool)

--- ConcurrencyExecutorService/src/Main.java
+++ ConcurrencyExecutorService/src/Main.java
@@ -1,19 +1,27 @@
+import java.util.concurrent.ScheduledExecutorService;
+
 /**
  * Types of threads(4 basics)
  * FixedThreadPool (Fixed number of threads(safe - blocking queue)
  * CachedThreadPool (Not have fixed number, not have queue, have synchronous queue), uses free threads or create a thread if dont have a free one, kill threads idle when more than 60 seconds wth no execution
  * ScheduledThreadPool (Tasks that want to schedule after certain a delay/rate) more threads area created if required - Uses a delay queue
- * SingleThreadExecutor
+ * SingleThreadExecutor (Same as fixedThreadPool, but size is only one thread) recreates if is killed because of the task
  */
 
 public class Main {
     public static void main(String[] args) {
         // FixedThreadPool
-        FixedThreadPool runFixedThreadPool = new FixedThreadPool();
-        runFixedThreadPool.execute();
+//        FixedThreadPool runFixedThreadPool = new FixedThreadPool();
+//        runFixedThreadPool.execute();
 
         // CachedThreadPool
-        CachedThreadPool cachedThreadPool = new CachedThreadPool();
-        cachedThreadPool.execute();
+//        CachedThreadPool cachedThreadPool = new CachedThreadPool();
+//        cachedThreadPool.execute();
+
+        // ScheduledThreadPool
+        ScheduledThreadPool scheduledThreadPool = new ScheduledThreadPool();
+        // task that needs to run
+        // based on the schedule
+        scheduledThreadPool.execute();
     }
 }
---
+++ ConcurrencyExecutorService/src/ScheduledThreadPool.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledThreadPool {
+    public void execute(){
+        // for scheduling of tasks
+        try(ScheduledExecutorService service = Executors.newScheduledThreadPool(10)){
+            // task to run after 10 seconds delay
+            service.schedule(new Task(), 10, TimeUnit.SECONDS);
+
+            // task to run repeatedly every 10 seconds
+            // service.scheduleAtFixedRate(new Task(), 15, 10, TimeUnit.SECONDS);
+
+            // task to run repeatedly 10 seconds after previous task completes
+            // service.scheduleWithFixedDelay(new Task(), 15, 10, TimeUnit.SECONDS);
+        }
+    }
+}
