b973024d580af3d465550be833e7ad71b92e4f0d
mateuslopes92 <mateuslopes92@gmail.com>
2026-02-11 00:12:36 -0300

    ConcurrencyExecutorService: Callable: Callable task

---
+++ ConcurrencyExecutorService/src/CallableTask.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+// As runnable dont return anything to return something we can use Callable
+public class CallableTask implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return new Random().nextInt();
+    }
+}
