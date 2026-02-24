5e1437ff5adaed19ffc55df537e3e8801787b30a
mateuslopes92 <mateuslopes92@gmail.com>
2026-02-23 22:38:16 -0300

    ConcurrencyExecutorService: Callable: Callable example using future refactor

--- ConcurrencyExecutorService/src/CallableExample.java
+++ ConcurrencyExecutorService/src/CallableExample.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,20 +11,29 @@ public class CallableExample {
 
     public static void run(){
         // Submit the tasks for execution
-        Future<Integer> future = serivce.submit(new CallableTask());
+        List<Future> allFutures = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            Future<Integer> future = serivce.submit(new CallableTask());
+            allFutures.add(future);
+        }
+        // 100 futures, 100 placeholders
+
 
         // Perform some unrelated operations
 
-        // 3 Sec
-        Integer result = null; // blocking
-        try {
-            result = future.get();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        // 100 seconds
+        for(int i = 0; i < 100; i++){
+            Future<Integer> future = allFutures.get(i);
+            Integer result = null;
+            try {
+                result = future.get();  // blocking
+                System.out.println("Result of future #" + i + "=" + result);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
 
-        System.out.println("Thread name: " + Thread.currentThread().getName() + " Task return: " + result);
+        }
     }
 }
