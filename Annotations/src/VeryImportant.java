745906abf33305bb7c3dcc25a8f9a86b19508e54
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-09 20:31:57 -0300

    Annotations: update custom annotation with new annotations(Retention)

--- Annotations/src/VeryImportant.java
+++ Annotations/src/VeryImportant.java
@@ -1,7 +1,10 @@
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 // To create an annotation is just use @interface instead of class
-@Target({ElementType.TYPE, ElementType.METHOD}) // Target specify which kind of java element can use the annotation
+@Target({ElementType.TYPE, ElementType.METHOD}) // Target specifies which kind of Java element can use the annotation
+@Retention(RetentionPolicy.RUNTIME) // Retention defines how long the annotation is kept. RUNTIME means it is available during program execution and can be accessed via reflection
 public @interface VeryImportant {
 }
