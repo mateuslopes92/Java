5ccf5a284d03b0fc7a9ce1975d555e411833dcce
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-09 20:28:54 -0300

    Annotations: update custom annotation with new annotations(Target))

--- Annotations/src/VeryImportant.java
+++ Annotations/src/VeryImportant.java
@@ -1,3 +1,7 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 // To create an annotation is just use @interface instead of class
+@Target({ElementType.TYPE, ElementType.METHOD}) // Target specify which kind of java element can use the annotation
 public @interface VeryImportant {
 }
