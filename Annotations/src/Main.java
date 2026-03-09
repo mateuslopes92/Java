6ae32c5a9d65181d2e11c4fd82e4dd3eda7073da
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-09 20:42:29 -0300

    Annotations: Processing annotation in code simple

--- Annotations/src/Main.java
+++ Annotations/src/Main.java
@@ -2,6 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         Dog myDog = new Dog("Jayne");
+        Cat myCat = new Cat("Mary");
+
+        // Processing Annotation
+        // Look at the class and check weather the class was marked with VeryImportant annotation
+        // We can do whatever we want if sees the annotation on that class
+        // The class has the annotation not the object
+//       if (myDog.getClass().isAnnotationPresent(VeryImportant.class)){
+        if (myCat.getClass().isAnnotationPresent(VeryImportant.class)){
+           System.out.println("This thing is very important!");
+       } else {
+           System.out.println("This thing is not very important!");
+       }
     }
 }
