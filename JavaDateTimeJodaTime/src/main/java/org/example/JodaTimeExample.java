3f9cabd9fb8af00a2d7939b8341663b8125788bd
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-21 15:26:13 -0300

    DateTimeJodaTime: Added JodaTimeExample code

---
+++ JavaDateTimeJodaTime/src/main/java/org/example/JodaTimeExample.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.joda.time.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class JodaTimeExample {
+
+    public static void main(String[] args) {
+
+        // Current date and time
+        DateTime now = DateTime.now();
+        System.out.println("Now: " + now);
+
+        // Specific date
+        LocalDate date = new LocalDate(2026, 3, 21);
+        System.out.println("Date: " + date);
+
+        // Timezone
+        DateTime zoned = DateTime.now(DateTimeZone.forID("America/Sao_Paulo"));
+        System.out.println("Brazil Time: " + zoned);
+
+        // Formatting
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+        System.out.println("Formatted: " + now.toString(formatter));
+
+        // Adding time
+        DateTime future = now.plusDays(5).plusHours(3);
+        System.out.println("Future: " + future);
+
+        // Difference
+        Duration duration = new Duration(now, future);
+        System.out.println("Hours difference: " + duration.getStandardHours());
+    }
+}
