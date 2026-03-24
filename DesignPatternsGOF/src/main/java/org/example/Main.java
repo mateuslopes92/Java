baef69f6268bc53b5982dacaa5140289787bcf4d
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-23 23:18:20 -0300

    Creational(Singleton): Refactor on Main class with Singleton example

--- DesignPatternsGOF/src/main/java/org/example/Main.java
+++ DesignPatternsGOF/src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import org.example.creational.SingletonExample;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Design Patterns in Java!");
 
-
+        SingletonExample.main(args);
     }
 }
