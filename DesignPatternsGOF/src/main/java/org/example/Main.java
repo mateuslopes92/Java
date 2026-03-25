9ee6df7a27a7b43dcc5428dd2ea7bae172d728e2
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-25 00:17:46 -0300

    Creational(Factory): Add Factory to main

--- DesignPatternsGOF/src/main/java/org/example/Main.java
+++ DesignPatternsGOF/src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.example.creational.SingletonExample;
+import org.example.creational.FactoryExample;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Design Patterns in Java!");
 
         SingletonExample.main(args);
+        FactoryExample.main(args);
     }
 }
