7e17bbdabc77811fe1b6172b9e5eda3de4d1e138
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-02 12:02:00 -0300

    Structural(Adapter): Added adpter pattern to main

--- DesignPatternsGOF/src/main/java/org/example/Main.java
+++ DesignPatternsGOF/src/main/java/org/example/Main.java
@@ -1,15 +1,20 @@
 package org.example;
 
 import org.example.creational.*;
+import org.example.structural.AdapterExample;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Design Patterns in Java!");
 
+        // Creational
         SingletonExample.main(args);
         FactoryExample.main(args);
         AbstractFactoryExample.main(args);
         BuilderExample.main(args);
         PrototypeExample.main(args);
+
+        // Structural
+        AdapterExample.main(args);
     }
 }
