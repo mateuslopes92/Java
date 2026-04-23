8ab87249fe8ab8fe84c1f9368427c7effb920ebb
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-23 00:24:18 -0300

    Java25NewFeatures(SealedClassesExample): Added SealedClassesExample class to main

--- Java25NewFeatures/src/main/java/org/example/Main.java
+++ Java25NewFeatures/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         VirtualThreadExample.main(args);
         PatternMatchingExample.main(args);
         RecordPatternExample.main(args);
+        SealedClassesExample.main(args);
     }
 }
