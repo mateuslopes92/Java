b57a31190bb70a7f69cdafa064fa6026d2ce77cb
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-22 23:39:20 -0300

    Java25NewFeatures(RecordPatternExample): Added RecordPatternExample to main

--- Java25NewFeatures/src/main/java/org/example/Main.java
+++ Java25NewFeatures/src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         VirtualThreadExample.main(args);
         PatternMatchingExample.main(args);
+        RecordPatternExample.main(args);
     }
 }
