8224ce61d9cef4a29a64aa629f90786adf129cd0
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-24 20:10:46 -0300

    Java25NewFeatures(StructuredConcurrencyExample): Added StructuredConcurrencyExample to main

--- Java25NewFeatures/src/main/java/org/example/Main.java
+++ Java25NewFeatures/src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         VirtualThreadExample.main(args);
         PatternMatchingExample.main(args);
         RecordPatternExample.main(args);
         SealedClassesExample.main(args);
+        StructuredConcurrencyExample.main(args);
     }
 }
