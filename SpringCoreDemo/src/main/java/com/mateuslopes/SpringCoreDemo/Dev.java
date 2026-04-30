d8a5959e4c28b9788f2d3221e95b6de0ef4e7fa0
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-30 09:00:00 -0300

    Spring(DependencyInjection): Dependency Injection with spring

---
+++ SpringCoreDemo/src/main/java/com/mateuslopes/SpringCoreDemo/Dev.java
@@ -0,0 +1,10 @@
+package com.mateuslopes.SpringCoreDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Building awesome things!");
+    }
+}
--- SpringCoreDemo/src/main/java/com/mateuslopes/SpringCoreDemo/SpringCoreDemoApplication.java
+++ SpringCoreDemo/src/main/java/com/mateuslopes/SpringCoreDemo/SpringCoreDemoApplication.java
@@ -1,13 +1,22 @@
 package com.mateuslopes.SpringCoreDemo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreDemoApplication.class, args);
+		ApplicationContext context;
+
+		context = SpringApplication.run(SpringCoreDemoApplication.class, args);
+
+		Dev obj = context.getBean(Dev.class);
+
+		obj.build();
+
 	}
 
 }
