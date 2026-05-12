f98efbb27003b946b1dcebf6069f382cdbbf9511
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-12 00:19:43 -0300

    SpringWebApp: Added more methods to product service add and getbyid

--- SpringWebApp/src/main/java/com/mateuslopes/SpringWebApp/service/ProductService.java
+++ SpringWebApp/src/main/java/com/mateuslopes/SpringWebApp/service/ProductService.java
@@ -3,20 +3,32 @@ package com.mateuslopes.SpringWebApp.service;
 import com.mateuslopes.SpringWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(1, "Iphone", 5000),
             new Product(2, "Camera", 7000),
             new Product(3, "Macbook", 10000)
-    );
+    ));
 
     public List<Product> getProducts(){
         return products;
     }
 
+    public Product getProductId(int prodId) {
+        return products
+                .stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst()
+                .get();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
 }
