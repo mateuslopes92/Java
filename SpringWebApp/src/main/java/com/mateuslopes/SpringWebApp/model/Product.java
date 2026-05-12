a822c02423f2c5cc79127c95439c0b3e9db6b70d
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-11 23:42:42 -0300

    SpringWebApp: Added model layer

---
+++ SpringWebApp/src/main/java/com/mateuslopes/SpringWebApp/model/Product.java
@@ -0,0 +1,14 @@
+package com.mateuslopes.SpringWebApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+
+    private int prodId;
+    private String prodName;
+    private int price;
+
+}
