b8916061836936cb94aeff8ac1dc672229e91947
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-19 00:13:10 -0300

    SpringWebApp: Update Product as Entity

--- SpringWebApp/src/main/java/com/mateuslopes/SpringWebApp/model/Product.java
+++ SpringWebApp/src/main/java/com/mateuslopes/SpringWebApp/model/Product.java
@@ -1,12 +1,19 @@
 package com.mateuslopes.SpringWebApp.model;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int prodId;
     private String prodName;
     private int price;
