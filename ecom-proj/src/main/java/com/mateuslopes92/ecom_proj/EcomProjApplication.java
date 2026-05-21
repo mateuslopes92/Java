6bee2cf4d3e0a648b2f951240f3ad560e542ef83
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-21 00:18:20 -0300

    SpringEcom: Format application

--- ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/EcomProjApplication.java
+++ ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/EcomProjApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcomProjApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcomProjApplication.class, args);
 	}
-
 }
