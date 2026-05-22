aebd693b3f8dcf54c4557c12ce961607e7126fd9
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-22 00:25:43 -0300

    SpringEcom: Added get all products controller method

--- ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/controller/ProductController.java
@@ -1,14 +1,30 @@
 package com.mateuslopes92.ecom_proj.controller;
 
+import com.mateuslopes92.ecom_proj.model.Product;
+import com.mateuslopes92.ecom_proj.service.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
-    @RequestMapping
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+
+    @RequestMapping("/")
     public String greet(){
         return "Hello";
+    }
+
+    @RequestMapping("/products")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
