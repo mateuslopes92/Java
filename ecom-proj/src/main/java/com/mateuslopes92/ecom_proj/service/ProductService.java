49ff95f971c9706459730280c02e19a17cdb80d7
mateuslopes92 <mateuslopes92@gmail.com>
2026-05-27 00:27:09 -0300

    SpringEcom: Changed product service to add product

--- ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/service/ProductService.java
+++ ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/service/ProductService.java
@@ -4,7 +4,9 @@ import com.mateuslopes92.ecom_proj.model.Product;
 import com.mateuslopes92.ecom_proj.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +21,12 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepository.save(product);
     }
 }
