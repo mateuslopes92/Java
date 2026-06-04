8a454a97170b4395c95282442949cfb486e9ca6c
mateuslopes92 <mateuslopes92@gmail.com>
2026-06-03 23:20:19 -0300

    SpringEcom: Changed controller for search

--- ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/mateuslopes92/ecom_proj/controller/ProductController.java
@@ -85,4 +85,11 @@ public class ProductController {
             return new ResponseEntity<>("Product not found!", HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        System.out.println("Searching with " + keyword);
+        List<Product> products = productService.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
