d39a5f91d20f7dc668104617615491d1fc30680d
mateuslopes92 <mateuslopes92@gmail.com>
2026-06-16 23:59:19 -0300

    SpringSecurity: Added JWT config on spring boot

--- SpringSecurity/src/main/java/com/mateuslopes92/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/mateuslopes92/SpringSecurity/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.mateuslopes92.SpringSecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,7 +27,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.csrf(customizer -> customizer.disable()).
-                authorizeHttpRequests(request -> request.anyRequest().authenticated()).
+                authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("register", "login")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).
                 httpBasic(Customizer.withDefaults()).
                 sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
     }
@@ -55,7 +63,11 @@ public class SecurityConfig {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 
-
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
     }
 }
