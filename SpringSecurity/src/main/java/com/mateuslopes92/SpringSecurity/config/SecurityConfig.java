a684a297919c568597b114cb56c79f6c80fbbb9a
mateuslopes92 <mateuslopes92@gmail.com>
2026-06-23 23:36:06 -0300

    SpringSecurity: Changed security config

--- SpringSecurity/src/main/java/com/mateuslopes92/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/mateuslopes92/SpringSecurity/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +23,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,7 +39,10 @@ public class SecurityConfig {
                                 .authenticated()
                 ).
                 httpBasic(Customizer.withDefaults()).
-                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+                sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
 
